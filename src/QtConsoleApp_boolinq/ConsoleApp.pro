cbb14f7d7112c5216837f3e5558ae585554539e4
Peter <sheraium@gmail.com>
2021-03-26 10:48:51 +0800

    新增 boolinq 測試專案

---
+++ src/QtConsoleApp_boolinq/ConsoleApp.pro
@@ -0,0 +1,17 @@
+QT += widgets
+INCLUDEPATH += $$PWD/../../include
+#LIBS += -L../../lib -l
+
+#HEADERS += \
+
+SOURCES += main.cpp
+
+CONFIG += console
+#CONFIG += staticlib
+#TEMPLATE = lib
+
+DESTDIR += ./bin
+TARGET = ConsoleApp
+
+HEADERS += \
+    boolinq.h
---
+++ src/QtConsoleApp_boolinq/main.cpp
@@ -0,0 +1,70 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QMap>
+
+#include "boolinq.h"
+
+using namespace std;
+using namespace boolinq;
+
+struct Man {
+    QString name;
+    int age;
+};
+
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+
+    int num[] = {1,2,3,4,5,6,7,8,9,10};
+
+    auto first = from(num).first();
+    qInfo()<< first;
+    auto anyGreaterThan5 = from(num).count([](const int &x){return x> 5;});
+    qInfo() << anyGreaterThan5;
+
+    Man Kevin;
+    Kevin.name = "Kevin";
+    Kevin.age = 14;
+
+    Man Anton;
+    Anton.name = "Anton";
+    Anton.age = 18;
+
+    Man Terra;
+    Terra.name = "Terra";
+    Terra.age = 17;
+
+
+    QMap<QString, Man> map;
+    map.insert("Kevin", Kevin);
+    map.insert("Anton", Anton);
+    map.insert("Kevin", Terra);
+
+    foreach(auto d, map){
+        qInfo() << d.name;
+    }
+
+    try {
+        auto data = from(map)
+                .select([](const Man &man){ return man; })
+                .toStdList();
+
+//        auto list = QList<QString>::fromStdList(data);
+
+        foreach(auto d, data){
+            qInfo() << d.name;
+        }
+
+    } catch (char* e){
+        qWarning() << "We caught a pointer to char* " << e;
+    } catch (exception const& e) {
+        qInfo()<< e.what();
+    }
+    catch (...){
+        qInfo()<<"Error";
+    }
+
+    return a.exec();
+}
+
